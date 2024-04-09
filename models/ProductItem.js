86f91adfd0a6f49813dde34dfbf661dac7913e8e
Aadiraj0762 <narayaneaadiraj@gmail.com>
2024-04-09 15:21:46 +0530

    latest backend push

---
+++ models/ProductItem.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const ProductItemSchema = new mongoose.Schema(
+  {
+    ProductItemId: {
+      type: String,
+      required: false,
+    },
+    // quantity: {
+    //   type: String,
+    //   required: true,
+    // },
+    // items: {
+    //   type: [
+    //     {
+    //       value: { type: String },
+    //       label: { type: String },
+    //     },
+    //   ],
+    //   required: false,
+    // },
+    title: {
+      type: Object,
+      required: true,
+    },
+    description: {
+      type: Object,
+      required: false,
+    },
+
+    image: {
+      type: Array,
+      required: false,
+    },
+    variants: { type: String, required: true },
+    // isCombination: {
+    //   type: Boolean,
+    //   required: false,
+    // },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+// module.exports = ProductItemSchema;
+
+const ProductItem = mongoose.model("ProductItem", ProductItemSchema);
+module.exports = ProductItem;
