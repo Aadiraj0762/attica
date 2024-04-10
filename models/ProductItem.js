f89183b3bed237bca6eccbf868fe93acdbed9379
Aadiraj0762 <narayaneaadiraj@gmail.com>
2024-04-10 09:56:17 +0530

    latest backend package push

--- models/ProductItem.js
+++ models/ProductItem.js
@@ -1,50 +1,110 @@
+// const mongoose = require("mongoose");
+
+// const ProductItemSchema = new mongoose.Schema(
+//   {
+//     ProductItemId: {
+//       type: String,
+//       required: false,
+//     },
+//     // itemsquantity: [
+//     //   {
+//     //     type: String,
+//     //     required: true,
+//     //   },
+//     // ],
+        
+//     selectedItems: [
+//       {
+//           item: { type: mongoose.Schema.Types.ObjectId, ref: "ProductItem" },
+//           quantity: { type: Number, default: 0 }
+//       }
+//   ],
+//   selectedQuantity: {
+//       type: Number,
+//       default: 0
+//   },
+//     items: {
+//       type: [
+//         {
+//           value: { type: String },
+//           label: { type: String },
+//         },
+//       ],
+//       required: false,
+//     },
+//     title: {
+//       type: Object,
+//       required: true,
+//     },
+//     description: {
+//       type: Object,
+//       required: false,
+//     },
+
+//     image: {
+//       type: Array,
+//       required: false,
+//     },
+//     variants: { type: String, required: true },
+//   },
+
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// // module.exports = ProductItemSchema;
+
+// const ProductItem = mongoose.model("ProductItem", ProductItemSchema);
+// module.exports = ProductItem;
 const mongoose = require("mongoose");
 
-const ProductItemSchema = new mongoose.Schema(
-  {
-    ProductItemId: {
-      type: String,
-      required: false,
-    },
-    itemsquantity: {
-      type: String,
-      required: true,
-    },
-    items: {
-      type: [
-        {
-          value: { type: String },
-          label: { type: String },
+const SelectedItemsSchema = new mongoose.Schema({
+    item: {
+        type: {
+            value: { type: String },
+            label: { type: String }
         },
-      ],
-      required: false,
+        required: true
     },
+    quantity: {
+        type: Number,
+        required: true
+    }
+});
+
+const ProductItemSchema = new mongoose.Schema({
+
+    itemsquantity: [{
+        type: String,
+        required: true,
+    }],
+
     title: {
-      type: Object,
-      required: true,
+        type: String,
+        required: true,
     },
     description: {
-      type: Object,
-      required: false,
+        type: String,
+        required: true,
     },
-
     image: {
-      type: Array,
-      required: false,
+        type: Array,
+        required: true,
     },
-    variants: { type: String, required: true },
-    // isCombination: {
-    //   type: Boolean,
-    //   required: false,
-    // },
-  },
-
-  {
-    timestamps: true,
-  }
-);
-
-// module.exports = ProductItemSchema;
+    variants: {
+        type: String,
+        required: true
+    },
+    selectedItems: [SelectedItemsSchema],
+    // selectedQuantity: {
+    //     type: Number,
+    //     default: 0
+    // }
+},
+{
+    timestamps: true
+});
 
 const ProductItem = mongoose.model("ProductItem", ProductItemSchema);
 module.exports = ProductItem;
